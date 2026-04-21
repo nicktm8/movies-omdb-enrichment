8b8e4176f38374630c1662ab35c073cd11b182d3
Nick Tem <nola234@icloud.com>
2026-04-21 22:20:48 +0200

    feat(api): add request logging before API call

--- src/omdb_client.py
+++ src/omdb_client.py
@@ -4,6 +4,7 @@ from config import API_KEY
 def get_movie_data(title, year, api_key=API_KEY):
     url = f'http://www.omdbapi.com/?apikey={api_key}&t={title}&y={year}'  
     response = requests.get(url)
+    print(f"Requesting: {title} ({year})")
     if response.status_code == 200:
         return response.json()
     else:
