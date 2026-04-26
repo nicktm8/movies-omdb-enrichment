87e9ea0ae61bdcaf53b20bdfb9273735c28d4469
Nick Tem <nola234@icloud.com>
2026-04-26 15:11:53 +0200

    refactor(api): extract query params and filter response fields

--- src/omdb_client.py
+++ src/omdb_client.py
@@ -2,11 +2,24 @@ import requests
 from config import API_KEY
 
 def get_movie_data(title, year, api_key=API_KEY):
-    url = f'http://www.omdbapi.com/?apikey={api_key}&t={title}&y={year}'  
-    response = requests.get(url)
+    url = f'http://www.omdbapi.com/'  
+    parameters = {
+        't': title,
+        'y': year,
+        'apikey': api_key
+    }
+    response = requests.get(url, params=parameters, timeout=4)
+    
     print(f"Requesting: {title} ({year})")
+    
     if response.status_code == 200:
-        return response.json()
+        data = response.json()
+        return {
+            "imdb_rating": data.get("imdbRating", "N/A"),
+            "actors": data.get("Actors", "N/A"),
+            "imdb_votes": data.get("imdbVotes", "N/A")
+        }
+        
     else:
         print(f"❌ Failed to fetch data for {title} ({year}): {response.status_code}")
         return None
