028c721f87d09c1d14b39c3f85a19b6268c926ab
Nick Tem <nola234@icloud.com>
2026-04-26 19:17:11 +0200

    feat: add main pipeline entry point

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
-def main():
-    print("Hello, world!")
+from extractor import extract
+from transformer import transform
+from load import save_to_xml
+from analysis import sort_movies_by_rating
 
-if __name__ == "__main__":
-    main()
+# Extract data from CSV
+csv_data = extract()
+
+# Transform data by enriching it with OMDb API
+movies = transform(csv_data)
+
+# Load data into the xml file
+save_to_xml(movies)
+
+# Sort movies by IMDb rating and display top 10
+sort_movies_by_rating(movies)
